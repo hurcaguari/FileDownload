ae9a897afe958dd5560a3f92eafb26112c3b8c6e
hurcaguari <hurcaguari@outlook.com>
2024-08-31 21:34:42 +0800

    main

--- main.py
+++ main.py
@@ -18,5 +18,6 @@ img_list  =[
     ['http://syxiaojuzi.com/manhua/4/{}.jpg'.format(str(i).zfill(2)) for i in range(1,28)]
 ]
 
-download(http_list,'C:\\Users\\Hurca\\Downloads\\mov')
-download(img_list,'C:\\Users\\Hurca\\Downloads\\img')
+if __name__ == "__main__":
+    download(http_list,'C:\\Users\\Hurca\\Downloads\\mov')
+    download(img_list,'C:\\Users\\Hurca\\Downloads\\img')
