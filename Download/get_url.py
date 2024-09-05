117df3aa5e34a3b0bafa0c9a8235e3399a8b7ba2
hurcaguari <hurcaguari@outlook.com>
2024-09-05 14:49:33 +0800

    01

--- Download/get_url.py
+++ Download/get_url.py
@@ -1,79 +1,87 @@
-'''
-文件下载类
-'''
-
+import logging
 from urllib import request
 from requests import get
-from os.path import join
-from os.path import exists
-from os.path import getsize
-from os.path import dirname
+from os.path import join, exists, getsize, dirname
 from os import makedirs
 from math import ceil
-from .get import get_property
-from .get import get_out_path
+from .get import get_property, get_out_path
 from .out_cmd import print_cmd
 
-
-
+logger = logging.getLogger(__name__)
 
 URL = ''
 perty = {}
 inx = 0
 
-def dow_list(http_list,path): # 列表参数处理
+def dow_list(http_list, path): # 列表参数处理
     global inx
     inx += 1
     path = get_out_path('')['path'] if not path else path
     for i in http_list:
-        pathup = dirname(i) if isinstance(i,str) else path
-        pathup = pathup.split('/')[-1] if isinstance(i,str) else path
-        dow_list(i,path) if isinstance(i,list) else dow_file(i,path+'\\'+ pathup)
-        
+        pathup = dirname(i) if isinstance(i, str) else path
+        pathup = pathup.split('/')[-1] if isinstance(i, str) else path
+        dow_list(i, path) if isinstance(i, list) else dow_file(i, join(path, pathup))
 
-def dow_file(url,file = ''): # 主入口
-    global URL,perty
+def dow_file(url, file=''): # 主入口
+    global URL, perty
     URL = url
-    perty = get_property(url) 
+    perty = get_property(url)
     perty['file']['path'] = file if file else perty['file']['path']
-    path = join(perty['file']['path'],perty['file']['name'])
+    path = join(perty['file']['path'], perty['file']['name'])
     if exists(path):
-        print_cmd(perty,100,Resume=True) if getsize(path) == perty['size']['bytes'] else resume(perty)
+        if getsize(path) == perty['size']['bytes']:
+            print_cmd(perty, 100, Resume=True)
+        else:
+            resume(perty)
     else:
-        file_load(perty) if not float(perty['size']['KB']) > 128 else file_load_max(perty)
-
+        if float(perty['size']['KB']) > 128:
+            file_load_max(perty)
+        else:
+            file_load(perty)
 
 def file_load(perty): # 小文件下载函数
+    logger.info(f"开始下载小文件: {perty['file']['name']}")
     print_cmd(perty)
-    path = join(perty['file']['path'],perty['file']['name'])
-    makedirs(perty['file']['path']) if not exists(perty['file']['path']) else None
-    request.urlretrieve(perty['url'],path) if perty['status'] == 200 else None
-    print_cmd(perty,100)
+    path = join(perty['file']['path'], perty['file']['name'])
+    create_directory(perty['file']['path'])
+    if perty['status'] == 200:
+        request.urlretrieve(perty['url'], path)
+    print_cmd(perty, 100)
+    logger.info(f"小文件下载完成: {perty['file']['name']}")
 
 def file_load_max(perty): # 大文件下载函数
-    
-    path = join(perty['file']['path'],perty['file']['name'])
-    makedirs(perty['file']['path']) if not exists(perty['file']['path']) else None
-    request.urlretrieve(perty['url'],path,reporthook=schedule) if perty['status'] == 200 else None
+    logger.info(f"开始下载大文件: {perty['file']['name']}")
+    path = join(perty['file']['path'], perty['file']['name'])
+    create_directory(perty['file']['path'])
+    if perty['status'] == 200:
+        request.urlretrieve(perty['url'], path, reporthook=schedule)
+    logger.info(f"大文件下载完成: {perty['file']['name']}")
 
-def schedule(blocknum,blocksize,totalsize,Resume = False): # 进度条对接函数
+def schedule(blocknum, blocksize, totalsize, Resume=False): # 进度条对接函数
     percent = totalsize / blocksize
     per = 100 / percent
     percent = ceil(blocknum * per)
     percent = 100 if 100 - percent < per else percent
-    print_cmd(perty,percent,Resume)
+    print_cmd(perty, percent, Resume)
 
 def resume(perty): # 断点续传下载
+    logger.info(f"开始断点续传下载: {perty['file']['name']}")
     block = 8192
-    path = join(perty['file']['path'],perty['file']['name'])
+    path = join(perty['file']['path'], perty['file']['name'])
     temp_size = getsize(path) if exists(path) else 0
     headers = {'Range': 'bytes=%d-' % temp_size}
-    r = get(perty['url'],stream=True,verify=False,headers=headers)
-    schedule(temp_size/block,block,perty['size']['bytes'],Resume = True)
-    with open(path,'ab') as f:
+    r = get(perty['url'], stream=True, verify=False, headers=headers)
+    schedule(temp_size / block, block, perty['size']['bytes'], Resume=True)
+    with open(path, 'ab') as f:
         for chunk in r.iter_content(chunk_size=block):
             if chunk:
                 temp_size += len(chunk)
                 f.write(chunk)
                 f.flush()
-                schedule(temp_size/block,block,perty['size']['bytes'])
+                schedule(temp_size / block, block, perty['size']['bytes'])
+    logger.info(f"断点续传下载完成: {perty['file']['name']}")
+
+def create_directory(path):
+    if not exists(path):
+        makedirs(path)
+        logger.info(f"创建目录: {path}")
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import logging
 from Download import download
 
-
+# 配置日志记录器
+logging.basicConfig(
+    level=logging.DEBUG,  # 设置日志级别
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # 设置日志格式
+    handlers=[
+        logging.FileHandler("app.log"),  # 将日志写入文件
+        logging.StreamHandler()  # 将日志输出到控制台
+    ]
+)
 
 http_list = [
     'http://syxiaojuzi.com/video/01.mp4',
@@ -16,13 +25,13 @@ img_list  =[
     ['http://syxiaojuzi.com/manhua/2/{}.jpg'.format(str(i).zfill(2)) for i in range(1,28)],
     ['http://syxiaojuzi.com/manhua/3/{}.jpg'.format(str(i).zfill(2)) for i in range(1,28)],
     ['http://syxiaojuzi.com/manhua/4/{}.jpg'.format(str(i).zfill(2)) for i in range(1,28)]
-    
 ]
 
 if __name__ == "__main__":
+    logging.info("开始下载视频文件")
     download(http_list,'C:\\Users\\Hurca\\Downloads\\mov')
+    logging.info("视频文件下载完成")
+
+    logging.info("开始下载图片文件")
     download(img_list,'C:\\Users\\Hurca\\Downloads\\img')
-
-
-
-        
+    logging.info("图片文件下载完成")
